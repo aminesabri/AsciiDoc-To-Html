262df4a194a1b8b2eb24f67a90e1bc7a7c7584d0
aminesabri <med.amine.sabri@gmail.com>
2016-02-05 12:04:22 +0100

    test HTML

---
+++ monscript.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#configuration
+git config --global user.email "moghitkacimi@gmail.com"
+git config --global user.name "kacimimoghite"
+
+ git clone --quiet https://${GH_TOKEN}@github.com/${GH_REF}  master > /dev/null
+
+
+
+ #add, commit and push files
+ git add -f .
+ git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
+ git push -fq origin master > /dev/null
+
+ echo -e "Deploy completed\n"
+
+ #fi
