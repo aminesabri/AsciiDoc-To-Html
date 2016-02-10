b4b2150470eaaf97120cfa686619b73b6f3b23ba
aminesabri <med.amine.sabri@gmail.com>
2016-02-10 16:13:47 +0100

    test generation html

--- monscript.sh
+++ monscript.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd $HOME
+
 #configuration
 git config --global user.email "med.amine.sabri@gmail.com"
 git config --global user.name "aminesabri"
@@ -13,7 +15,7 @@ cd TestHTML
  git add -f .
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
 #git push -fq origin master > /dev/null
-git push --force --quiet https://${GH_TOKEN}@${GH_REF} master > /dev/null
+git push -f -q https://${GH_TOKEN}@${GH_REF} master > /dev/null
 
  echo -e "Deploy completed\n"
 
